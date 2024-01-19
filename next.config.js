a0f1646b576eb17b73fb38e5da54b05881a298d4
Zaber Ahmed <zaber0305@gmail.com>
2024-01-19 23:35:31 +0600

    Removed some comments and updated configs

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    typedRoutes: true, //statically type check routes
+    typedRoutes: true,
   },
   logging: {
-    //logging the full api url when data is fetched and also show cache hit or miss
+
     fetches: {
       fullUrl: true,
     },
